b785539179e1170cbd4b381d9dca4b27b2c761db
Alexander Schlicker <alexander@schlickster.us>
2017-05-27 22:48:06 -0400

    update views to handle flash messages. fix emails

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'items#index'
+  devise_for :users, controllers: { }
+  root 'static#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/unconfirmed' => 'static#unconfirmed'
+
   resources :items do
     resources :replacements
   end
