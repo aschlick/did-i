db7d75e57661255f63e37eec436d902f2be9f344
Alexander Schlicker <alexander@schlickster.us>
2017-05-29 17:41:30 -0400

    try sendgrid with heroku in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,16 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'did-i.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain
+    enable_starttls_auto: true
+  }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = {
     host: 'https://www.did-i.com',
