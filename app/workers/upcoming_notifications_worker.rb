7ac3d81e647c82ea8a7d3c293876fdde4929cee0
Alexander Schlicker <alexander@schlickster.us>
2017-11-26 14:40:32 -0500

    fix spec and upcoming reminders email

--- app/workers/upcoming_notifications_worker.rb
+++ app/workers/upcoming_notifications_worker.rb
@@ -3,7 +3,7 @@ class UpcomingNotificationsWorker
 
   def perform(*args)
     Item.includes(:user)
-      .where("date_trunc('day', last_replaced_at + period) = (TIMESTAMP \'yesterday\')")
+      .where("date_trunc('day', last_replaced_at + period) = (TIMESTAMP \'tomorrow\')")
       .group_by(&:user)
       .each do |u, items|
         UserMailer.upcoming_reminder_email(u, items).deliver
