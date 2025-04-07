fc2578c06da0f538bf94002832e3402e06a25b5f
Alexander Schlicker <alexander@schlickster.us>
2025-04-06 21:31:14 -0400

    add sidekiq back in

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'turbolinks'
 gem 'devise'
 gem 'haml'
 gem 'haml-rails'
+gem 'sidekiq'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/puma.rb
+++ config/puma.rb
@@ -39,3 +39,17 @@ plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+
+x = nil
+on_worker_boot do
+  x = Sidekiq.configure_embed do |config|
+    # config.logger.level = Logger::DEBUG
+    config.queues = %w[critical default low]
+    config.concurrency = 2
+  end
+  x.run
+end
+
+on_worker_shutdown do
+  x&.stop
+end
